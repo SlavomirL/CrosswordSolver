82d24b04581b2a734daeaa8c742d9a8409370938
Slavomir Lobotka <slavomir.lobotka@yahoo.com>
2024-02-09 21:22:10 +0100

    feat: add radio buttons with dummy testing logic

--- src/main/java/com/crosswordsolver/controllers/HomeController.java
+++ src/main/java/com/crosswordsolver/controllers/HomeController.java
@@ -3,8 +3,9 @@ package com.crosswordsolver.controllers;
 import com.crosswordsolver.services.CrosswordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +17,16 @@ public class HomeController {
     @GetMapping("/home")
     public String displayCrosswordSolverHomePage() {
         return "home-view";
+    }
+
+    @PostMapping("home")
+    public String selectWordLength(@RequestParam(name = "wordLength", required = false) Integer wordLength, RedirectAttributes redirectAttributes) {
+        String displayString = crosswordService.testMethod(wordLength);
+        System.out.println(displayString);
+        redirectAttributes.addFlashAttribute("stringToShow", displayString);
+
+
+        return "redirect:/crossword-solver/home";
     }
 
 }
--- src/main/java/com/crosswordsolver/services/CrosswordService.java
+++ src/main/java/com/crosswordsolver/services/CrosswordService.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CrosswordService {
+
+    public String testMethod(int wordLength) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < wordLength; i++) {
+            sb.append(i);
+        }
+        return sb.toString();
+    }
 }
